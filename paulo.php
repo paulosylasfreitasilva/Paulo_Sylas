c1b4ada47b9cd53e14a655315b545db9d8ee80ef
aluno <aluno@lab-wbs.local>
2016-03-30 08:09:06 -0300

    hoje eu fiz um corpo em html e um codigo em php

---
+++ paulo.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<title>imagem em fundo de pagina html</title>
+	</head>
+	<body background="https://image.freepik.com/vetores-gratis/flores-azuis-claras-e-ondas-de-fundo-abstrato_91-6428.jpg">
+	
+	</body>
+<?php
+$num1;
+$num2 = 2;
+
+	
+	
+	
+for($num1 =1; $num1 < 100; $num1++){
+ if(($num1 % $num2) == 0) {
+ echo"<br/>Numero Par: ".$num1;
+ 
+ 
+ }
+
+
+}
+	
+?>
+</html>
